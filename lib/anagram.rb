9cdeedd8c0c1a8e3340ca59656ebcde019685897
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-05-02 05:45:20 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,25 +6,23 @@ class Anagram
     @word = word
   end
 
-
-  def match(array)
+def match(array)
   results = []
   letters_in_word= @word.split('')
   letters_count = Hash.new(0)
 
   letters_in_word.each do |characters|
   letters_count[characters] += 1
-
-end
+  end
 
   array.collect do |word|
     characters = word.split('')
       to_hash= characters.each_with_object(Hash.new(0)){|signs, count| count[signs]+=1}
-          if to_hash == letters_count
-            results << word
-          end
-     end
-    results
-  end
+        if to_hash == letters_count
+          results << word
+        end
+      end
+  results
+end
 
 end
